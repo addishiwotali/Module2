fb83ce6b0889cc9038a64fb644bf5d7eb7796b26
Addishiwot Ali <sosenamillion@yahoo.com>
2026-04-14 21:56:09 -0700

    Module2 exercise

---
+++ Module2/Program.cs
@@ -0,0 +1,109 @@
+﻿namespace Module2;
+using System.Collections.Generic; 
+
+class Program
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        
+        public int ID { get; set; }
+        
+        public List<double> Grades  = new List<double>();
+        
+        public static void AddGrade(Student student, double grade)
+        
+        {
+            student.Grades.Add(grade);
+        }
+        
+        public static void AddGrade(Student student, params double[] grades)
+        
+        {
+            student.Grades.AddRange(grades);
+        }
+        
+        public static double CalculateAverageGrade(Student student)
+        {
+            if (student.Grades.Count == 0)
+            {
+                return 0;
+            }
+
+            double total = 0;
+            
+            foreach (double grade in student.Grades)
+            {
+                total += grade;
+            }
+            
+            return total / student.Grades.Count;
+        }
+    
+    
+        static void Main(string[] args)
+        {
+            List<Student> students = new List<Student>();
+            
+            Student student1 = new Student();
+            student1.Name = "Marry";
+            student1.ID = 1;
+            
+            Student student2 = new Student
+            {
+                Name = "Bethy",
+                ID = 2
+            };
+            
+            Student student3 = new Student
+            {
+                Name = "Micheal",
+                ID = 3
+            };
+            
+            Student student4 = new Student();
+            student4.Name = "Mercy";
+            student4.ID = 4;
+            
+            students.Add(student1);
+            students.Add(student2);
+            students.Add(student3);
+            students.Add(student4);
+            
+
+            Student.AddGrade(student1, 94.3);
+            Student.AddGrade(student1, 90.0, 78.9, 95.4);
+
+            Student.AddGrade(student2, 88.5);
+            Student.AddGrade(student2, 92.0, 85.4);
+
+            Student.AddGrade(student3, 76.2);
+            Student.AddGrade(student3, 80.0, 82.3, 79.9);
+
+            Student.AddGrade(student4, 91.0);
+            Student.AddGrade(student4, 89.5, 93.2);
+
+            
+            foreach (Student student in students)
+            {
+                Console.WriteLine($"ID: {student.ID}");
+                Console.WriteLine($"Name: {student.Name}");
+                Console.Write("Grades: ");
+
+                
+                foreach (double grade in student.Grades)
+                {
+                    Console.Write($"{grade} ");
+                }
+                
+                double average = Student.CalculateAverageGrade(student);
+                Console.WriteLine($"\nAverage Grade: {average:F2}");
+                Console.WriteLine("-----------------------------------");
+            }
+
+            
+        }
+    }
+}
+
+    
